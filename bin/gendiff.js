91e31c71e358d21be985c5d230f22592269498ba
Anders <anders.yourdev@gmail.com>
2022-11-18 13:10:43 +0200

    addded arguments, options, action to cli. the initial structure of the library module is created

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,9 +1,15 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
+import gendiff from '../src/index.js';
 
-program.version('1.0.0', '-V, --version', 'output the version number')
+program
+  .version('1.0.0', '-V, --version', 'output the version number')
   .description('Usage: gendiff [options]')
-  .description('Compare two configuration files and shows a difference.');
-
-program.parse();
+  .description('Compare two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format', 'default')
+  .action((filepath1, filepath2, options) => {
+    console.log(gendiff(filepath1, filepath2, options.format));
+  })
+  .parse();
---
+++ src/index.js
@@ -0,0 +1,3 @@
+const gendiff = (filepath1, filepath2, format = 'default') => ([filepath1, filepath2, format]);
+
+export default gendiff;
